7afd30945db4e253bc2967c457b8177f48b371cc
Hiroaki Suzuki <zafiel000@gmail.com>
2017-01-06 14:13:19 +0900

    default commit

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,20 @@
 #-*- coding: utf-8 -*-
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 
 def hello(name="world"):
     print("Hello %s" % name)
+
+def test():
+    """
+    confirmはプロンプト上にY/nの選択肢を表示する
+    capture=Trueにするとコマンドの結果を見れる. .failed or return_codeで
+    """
+
+    with settings(warn_only=True):
+        result = local("nosetests", capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting ad user request.")
 
 def commit(message="default commit"):
     local("git add -p && git commit -m \"%s\"" % message)
@@ -10,3 +22,7 @@ def commit(message="default commit"):
 def push():
     local("git push origin master")
 
+def prepare_deploy():
+    test()
+    commit()
+    push()
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("test app")
+from unittest import TestCase
+from nose.tools import ok_, eq_
+
+def sum(a, b):
+    return a + b
+
+class HogeTest(TestCase):
+    def test_sum(self):
+        eq_(sum(1, 2), 3)
+        eq_(sum(3, 4), 7)
+        eq_(sum(3, 4), 7)
+        eq_(sum(3, 3), 6)
