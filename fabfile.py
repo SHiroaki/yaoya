2b265d9357f96ec268f7c5ec5b27e9e1dd5c70b9
Hiroaki Suzuki <zafiel000@gmail.com>
2017-01-06 13:49:52 +0900

    test

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,9 @@ from fabric.api import local
 def hello(name="world"):
     print("Hello %s" % name)
 
+def commit(message="default commit"):
+    local("git add -p && git commit -m \"%s\"" % message)
+
+def push():
+    local("git push origin master")
+
